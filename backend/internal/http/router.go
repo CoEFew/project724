945dc2ac89d24ac58292428371974e5ead255b50
CoEFew <surasak.nk24@gmail.com>
2025-09-09 01:03:16 +0700

    update AllowedOrigins

--- backend/internal/http/router.go
+++ backend/internal/http/router.go
@@ -4,10 +4,19 @@ import (
 	"my-app-backend/internal/http/handlers"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func NewRouter() *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://project724-frontend.onrender.com"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 	r.Get("/health", handlers.Health)
 	r.Get("/api/quiz", handlers.GetQuiz)
 	r.Post("/api/quiz/check", handlers.CheckQuiz)
